3cc88c9ef85c344318df2e00c675812259842b49
Paulo Jr <paulolds086543@gmail.com>
2024-07-15 22:22:11 -0300

    Fim

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,12 +2,14 @@
 const form = document.querySelector(".form-contato")
 const footer = document.querySelector(".footer")
 
+// Função para enviar o formulário
 form.addEventListener("submit", (event) => {
   event.preventDefault()
   alert("Contato enviado com sucesso!")
   form.reset()
 })
 
+// inserindo o ano no rodape
 const year = new Date().getFullYear()
 const footerText = `Criado por Paulo ©️${year}`
 
